64972dfba3e2b830fc54ecbb084d20a7fa84eb24
wahidquadri <dhurwahidquadri@Dhurs-MacBook-Air.local>
2024-01-05 23:07:09 +0530

    test3

---
+++ Arrays/array-methods/list.py
@@ -0,0 +1,36 @@
+l = [10,20,30,"vahid",30,True]
+'''l.pop()          # this will remove the last value given in the above list
+print(l)'''
+
+'''l.remove(10)     # this will delete the certain value in the above list
+print(l)'''
+
+'''l.clear()        # this will clear all the value in the above list
+print(l)'''
+
+'''l.append(23)     # this will add the value in the end of the above list
+print(l)'''                             
+
+'''l.insert(3,"VK") # this will add the value in the certain place by mentioning the index value 
+print(l)'''
+
+'''l.reverse()         # this will reverse the given data in the above list 
+print(l)'''
+
+'''li = [1,2,4,9,6,8,40,20]  # the output will get in ascending order 
+li.sort()
+print(li)'''
+
+# print(l.count(30))    # this will show repeated value count 
+
+
+#print(l.index(30))   #this will show the certain value of index place
+
+
+count=0
+i=0
+while i<=len(l)-1:
+    count=i
+    i=i+1
+print(count)
+ 
--- Python basics/one.py
+++ Python basics/one.py
@@ -1,14 +1,14 @@
-# print 1 to 10 num 
+'''# print 1 to 10 num 
 #for x in range(1,11):
 #   print (x)
 
-'''for x in range(11,1,-5):
+for x in range(11,1,-5):
     print(x)'''
 
 '''for x in range(10,110,10):
     print(x)'''
 
-l = ["vahid","Siddu","tara"]
+'''l = ["vahid","Siddu","tara"]
 print(l)
 for x in l:
     print(x)
@@ -22,4 +22,14 @@ for ename in enames:
     print(ename)
 
 enames[1]='khan'
-print(enames)
+print(enames)'''
+
+
+a = 495
+b = 298
+c = a+b
+print(a+b)
+if(c%2==0):
+    print("Even")
+else:
+    print("ODD")
